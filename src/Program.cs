42e0f3ee131ef17bd1c906ac5ec00c4ef96490ac
Antao Almada <antao.almada@gmail.com>
2019-10-17 09:12:22 +0100

    Add source

---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿using Intel.RealSense;
+using System;
+
+namespace RealSenseExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pipe = new Pipeline();
+            pipe.Start();
+
+            while (true)
+            {
+                using var frames = pipe.WaitForFrames();
+                using var depth = frames.DepthFrame;
+                Console.WriteLine($"The camera is pointing at an object {depth.GetDistance(depth.Width / 2, depth.Height / 2)} meters away\t");
+
+                Console.SetCursorPosition(0, 0);
+            }
+        }
+    }
+}
